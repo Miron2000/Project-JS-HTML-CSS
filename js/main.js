45797534815c031d2fe6d462104df598675f7755
miron <miron.ivasch@gmail.com>
2019-11-07 21:15:29 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	$('.slider_inner').slick({
+		nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+		infinite: false
+
+
+	});
+
+
+});
+
